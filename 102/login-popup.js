ea0c5e195019fca40059716a67760d131de24e85
syntrofin <ravigo@gmail.com>
2026-03-09 16:09:37 -0400

    Update login-popup.js

--- 102/login-popup.js
+++ 102/login-popup.js
@@ -1,12 +1,13 @@
-document.body.insertAdjacentHTML('afterbegin', `
-<div class="login">
-    <h1>Syntrofin Login!!</h1>
-    <div class="login-container">
-        <input id="user" placeholder="User ID" autocomplete="username">
-        <input id="pass" type="password" placeholder="Password" autocomplete="current-password">
-        <button id="login-btn">Login</button>
-    </div>
-    <div id="test-msg" style="color: red; margin-top: 10px;"></div>
-</div>
-`);
+document.body.insertAdjacentHTML('afterbegin', 
+    '<div class="login">' +
+    '    <h1>Syntrofin Login!!</h1>' +
+    '    <div class="login-container">' +
+    '        <input id="user" placeholder="User ID" autocomplete="username">' +
+    '        <input id="pass" type="password" placeholder="Password" autocomplete="current-password">' +
+    '        <button id="login-btn">Login</button>' +
+    '    </div>' +
+    '    <div id="test-msg" style="color: red; margin-top: 10px;"></div>' +
+    '</div>'
+);
+
 
